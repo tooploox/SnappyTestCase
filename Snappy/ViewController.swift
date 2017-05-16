197997717fda7327b4d13d6346593fc6185e23b4
Przemysław Stasiak <stasiak89@gmail.com>
2017-05-16 13:26:00 +0200

    Added pods - FBSnapshotTestCase.

---
+++ Snappy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snappy
+//
+//  Created by Przemysław Stasiak on 16.05.2017.
+//  Copyright © 2017 Tooploox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
