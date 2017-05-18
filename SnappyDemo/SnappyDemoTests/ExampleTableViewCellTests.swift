92bb05d33fdc6752334413bb62e0fd0a21fe31a7
Przemysław Stasiak <stasiak89@gmail.com>
2017-05-18 16:43:48 +0200

    Example of snapshot testing UITableViewCell.

---
+++ SnappyDemo/SnappyDemoTests/ExampleTableViewCellTests.swift
@@ -0,0 +1,41 @@
+//
+//  Created by Przemysław Stasiak on 18.05.2017.
+//  Copyright © 2017 Tooploox. All rights reserved.
+//
+
+import XCTest
+import Snappy
+import FBSnapshotTestCase
+@testable import SnappyDemo
+
+class ExampleTableViewCellTests: FBSnapshotTestCase {
+    
+    let container = UIWindow()
+    lazy var cell: ExampleTableViewCell = {
+        let nib = UINib(nibName: String(describing: ExampleTableViewCell.self), bundle: Bundle.main)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! ExampleTableViewCell
+        view.bounds = CGRect(
+            origin: .zero,
+            size: CGSize(
+                width: self.container.bounds.size.width,
+                height: 80.0
+            )
+        )
+        view.autoresizingMask = [.flexibleWidth]
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    override func setUp() {
+        super.setUp()
+        recordMode = false
+        
+        container.makeKeyAndVisible()
+        container.addSubview(cell)
+    }
+    
+    func testAllWidths() {
+        verifyViewSnaps(DeviceRack.iPhone.all.portrait.uniqueWidths, view: cell, container: container)
+    }
+    
+}
