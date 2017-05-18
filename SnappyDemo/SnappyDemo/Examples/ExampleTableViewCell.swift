d75af162d9b3891b4d30f3b1989e6860ad831038
Przemysław Stasiak <stasiak89@gmail.com>
2017-05-18 16:43:48 +0200

    Example of snapshot testing UITableViewCell.

---
+++ SnappyDemo/SnappyDemo/Examples/ExampleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExampleTableViewCell.swift
+//  SnappyDemo
+//
+//  Created by Przemysław Stasiak on 18.05.2017.
+//  Copyright © 2017 Tooploox. All rights reserved.
+//
+
+import UIKit
+
+class ExampleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
